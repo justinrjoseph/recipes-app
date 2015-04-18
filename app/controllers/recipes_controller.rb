84e0a9927004280a6a9eec034ad7119e4bf98180
Justin Joseph <justin.joseph0@yahoo.com>
2015-04-18 20:46:44 +0000

    Built like/dislike system

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
    
    def index
-      @recipes = Recipe.all 
+      @recipes = Recipe.all.sort_by { |likes| likes.thumbs_up_total }.reverse
    end
    
    def show
@@ -37,6 +37,19 @@ class RecipesController < ApplicationController
       else
          render :edit
       end
+   end
+   
+   def like
+      @recipe = Recipe.find(params[:id])
+      like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+      if like.valid? && params[:like] == 'true'
+         flash[:success] = "You liked #{@recipe.chef.chefname}'s #{@recipe.name}!"
+      elsif like.valid? && params[:like] == 'false'
+         flash[:danger] = "You don't like #{@recipe.chef.chefname}'s #{@recipe.name}"
+      else
+         flash[:warning] = "You may like/dislike a recipe only once!"
+      end
+      redirect_to :back
    end
    
    private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get '/home' => 'pages#home'
-  resources :recipes
+  resources :recipes do
+    member do
+      post 'like'
+    end
+  end
 end
