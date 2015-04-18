f4a084037016bbcc455214dca031cd68d0cc6734
Justin Joseph <justin.joseph0@yahoo.com>
2015-04-18 22:55:52 +0000

    Added chef registration and authentication

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -7,4 +7,6 @@ class Chef < ActiveRecord::Base
     validates :chefname, presence: true, length: { minimum: 3, maximum: 40 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
+    
+    has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  
   get '/home' => 'pages#home'
+  
   resources :recipes do
     member do
       post 'like'
     end
   end
+  
+  resources :chefs, except: [:new]
+  
+  get '/register' => 'chefs#new'
+
 end
